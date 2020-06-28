5d823024a29fbeae070d0fc2ebce89f327211ec4
Guru K <mukhthakannan@gmail.com>
2020-06-29 01:03:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   return render_template('index2.html')
+
+@app.route('/register')
+def register():
+   return render_template('index2.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
